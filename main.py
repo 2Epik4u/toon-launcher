a93e02dd99e75705391c85c1ff210d2e9f815c2a
2Epik4u <justinabossmlg@gmail.com>
2024-12-16 17:03:59 -0500

    tighten up some ui stuff

--- main.py
+++ main.py
@@ -1,11 +1,12 @@
 import sys
-from PyQt6 import QtWidgets, uic
-from PyQt6.QtWidgets import QApplication, QMainWindow, QPushButton, QWidget, QLabel, QTextEdit, QLineEdit
+from PyQt6 import uic
+from PyQt6.QtWidgets import QApplication, QMainWindow, QPushButton, QLineEdit, QCheckBox, QComboBox
 import platform
 import subprocess
 import requests
 import os 
 import json
+import keyring
 
 TTR_LOGIN_API = "https://www.toontownrewritten.com/api/login?format=json"
 TTR_PATCHER_URL = "https://download.toontownrewritten.com/patches/"
@@ -33,14 +34,16 @@ class MainWindow(QMainWindow):
 
         # TTR Widgets
         self.button = self.findChild(QPushButton, "playbutton")
-        self.username = self.findChild(QLineEdit, "username")
-        self.password = self.findChild(QLineEdit, "password")
+        self.username = self.findChild(QComboBox, "username")
+        self.password = self.findChild(QLineEdit, "password_3")
+        self.saveTTR = self.findChild(QCheckBox, "checkBox_2")
 
         # Clash Widgets
         self.buttonclash = self.findChild(QPushButton, "playbuttonclash")
-        self.usernameclash = self.findChild(QLineEdit, "username_2")
+        self.usernameclash = self.findChild(QComboBox, "username_clash")
         self.passwordclash = self.findChild(QLineEdit, "password_2")
         self.friendlyname = self.findChild(QLineEdit, "pcname")
+        self.saveTTCC = self.findChild(QCheckBox, "checkBox")
 
         self.button.clicked.connect(self.playTTR)
         self.buttonclash.clicked.connect(self.playTTCC)
@@ -52,24 +55,24 @@ class MainWindow(QMainWindow):
         
 
     def playTTR(self):
-        self.usernamevalue = self.username.text()
+        self.usernamevalue = self.username.currentText()
         self.passwordvalue = self.password.text()
         data = {"username": self.usernamevalue,
                 "password": self.passwordvalue}
         headers = {"Content-type": "application/x-www-form-urlencoded"}
         r = requests.post(TTR_LOGIN_API, headers=headers, data=data).json()
-        self.load_settings()
-        if self.settings.get("DEBUG", False):
-            print(r)
         GAMESERVER = r['gameserver']
         COOKIE = r['cookie']
+        if self.saveTTR == True:
+            keyring.set_password("system", "{self.passwordvalue}", "{self.passwordvalue}")
+        
         if platform.system() == 'Windows':
             os.chdir('games/ttr')
             subprocess.run(r'./TTREngine64.exe', env=dict(os.environ, TTR_GAMESERVER=GAMESERVER, TTR_PLAYCOOKIE=COOKIE)) # TODO: check if user is x64 or x86. if x86 change executable
 
 
     def playTTCC(self):
-        self.username = self.usernameclash.text()
+        self.username = self.usernameclash.currentText()
         self.password = self.passwordclash.text()
         self.friendly = self.friendlyname.text()
         data = {"username": self.username,
@@ -77,18 +80,11 @@ class MainWindow(QMainWindow):
                 "friendly": self.friendly}
         headers = {"User-Agent:" "Toonlauncher/1.0.0"
                    }
-        # you're only supposed to call the register api once, so make a little note on settings that we've already registered.
-        if self.settings.get("TTC_REGISTER", False):
-            print()
         r = requests.post(TTCC_REGISTER_API, data=data).json()
-        if settings.DEBUG == True:
-            print(r)
         token = r['token']
         headers = {"User-Agent": "Toonlauncher/1.0.0",
                    "Authorization": f"{token}"}
         rl = requests.post(TTCC_LOGIN_API, headers=headers, data=data).json()
-        if DEBUG == True:
-            print(rl)
         GAMESERVER = rl['gameserver']
         COOKIE = rl['cookie']
         if platform.system() == 'Windows':
