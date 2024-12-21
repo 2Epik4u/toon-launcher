2399e5f6bdc1bd5f138423b7fa69a595dde39e4c
2Epik4u <justinabossmlg@gmail.com>
2024-12-21 15:45:13 -0500

    push whatever

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import sys
 from PyQt6 import uic
-from PyQt6.QtWidgets import QApplication, QMainWindow, QPushButton, QLineEdit, QCheckBox, QComboBox
+from PyQt6.QtWidgets import QApplication, QMainWindow, QPushButton, QLineEdit, QCheckBox, QComboBox, QMessageBox
 import platform
 import subprocess
 import requests
@@ -9,19 +9,19 @@ import json
 import keyring
 
 TTR_LOGIN_API = "https://www.toontownrewritten.com/api/login?format=json"
-TTR_PATCHER_URL = "https://download.toontownrewritten.com/patches/"
 TTCC_LOGIN_API = "https://corporateclash.net/api/launcher/v1/login"
 TTCC_REGISTER_API = "https://corporateclash.net/api/launcher/v1/register"
+
 
 class MainWindow(QMainWindow):
     def __init__(self):
         super().__init__()
 
         if not os.path.exists('settings.json'):
-            Settings = {
+            settings = {
                 "DEBUG": 'True'
             }
-            f = json.dumps(Settings)
+            f = json.dumps(settings)
             file = open("settings.json", "a")
             file.write(f)
             file.close()
@@ -36,13 +36,7 @@ class MainWindow(QMainWindow):
         self.username = self.findChild(QComboBox, "username")
         self.password = self.findChild(QLineEdit, "password_3")
         self.saveTTR = self.findChild(QCheckBox, "checkBox_2")
-        # Load saved accounts
-        credentials = keyring.get_credential("toon-launcher", None)
-        if credentials is not None:
-            username = credentials.username
-            password = credentials.password
-        self.username.addItem(username)
-        self.password.setText(password)
+
         # Clash Widgets
         self.buttonclash = self.findChild(QPushButton, "playbuttonclash")
         self.usernameclash = self.findChild(QComboBox, "username_clash")
@@ -53,11 +47,25 @@ class MainWindow(QMainWindow):
         self.button.clicked.connect(self.playTTR)
         self.buttonclash.clicked.connect(self.playTTCC)
 
+        # Load saved accounts
+        credentialsttr = keyring.get_credential("toon-launcher-ttr", None)
+        credentialsttcc = keyring.get_credential("toon-launcher-ttcc", None)
+        if credentialsttr is not None:
+            username = credentialsttr.username
+            password = credentialsttr.password
+            self.username.addItem(username)
+            self.password.setText(password)
+        if credentialsttcc is not None:
+            username = credentialsttcc.username
+            password = credentialsttr.password
+            self.usernameclash.addItem(username)
+            self.passwordclash.setText(password)
+
+
     def load_settings(self):
         with open("settings.json", 'r') as f:
             self.settings = json.load(f)
 
-        
 
     def playTTR(self):
         self.usernamevalue = self.username.currentText()
@@ -66,13 +74,26 @@ class MainWindow(QMainWindow):
                 "password": self.passwordvalue}
         headers = {"Content-type": "application/x-www-form-urlencoded"}
         r = requests.post(TTR_LOGIN_API, headers=headers, data=data).json()
-        GAMESERVER = r['gameserver']
-        COOKIE = r['cookie']
+        print(r)
+        if r['success'] == 'true':
+            gameserver = r['gameserver']
+            cookie = r['cookie']
+            patch_manifest = r['manifest']
+            ttr_manifest = f"https://cdn.toontownrewritten.com{patch_manifest}"
+        if r['success'] == 'false':
+            errorcode = r['banner']
+            QMessageBox.critical(self, "Yipes! ", f"{errorcode}")
+            return
+        if r['success'] == 'partial':
+            resptoken = r['responseToken']
+            uic.loadUi('assets/tfa.ui', self)
+
+
         if self.saveTTR.isChecked():
-            keyring.set_password("toon-launcher", str(self.usernamevalue), str(self.passwordvalue))
+            keyring.set_password("toon-launcher-ttr", str(self.usernamevalue), str(self.passwordvalue))
         if platform.system() == 'Windows':
             os.chdir('games/ttr')
-            subprocess.run(r'./TTREngine64.exe', env=dict(os.environ, TTR_GAMESERVER=GAMESERVER, TTR_PLAYCOOKIE=COOKIE)) # TODO: check if user is x64 or x86. if x86 change executable
+            subprocess.Popen(r'./TTREngine64.exe', env=dict(os.environ, TTR_GAMESERVER=gameserver, TTR_PLAYCOOKIE=cookie)) # TODO: check if user is x64 or x86. if x86 change executable
 
 
     def playTTCC(self):
@@ -86,6 +107,8 @@ class MainWindow(QMainWindow):
                    }
         r = requests.post(TTCC_REGISTER_API, data=data, headers=headers).json()
         token = r['token']
+        if self.saveTTCC.isChecked():
+            keyring.set_password("toon-launcher-ttcc", str(self.username), str(self.password))
         headers = {"User-Agent": "Toonlauncher/1.0.0",
                    "Authorization": f"{token}"}
         rl = requests.post(TTCC_LOGIN_API, headers=headers, data=data).json()
@@ -93,7 +116,8 @@ class MainWindow(QMainWindow):
         COOKIE = rl['cookie']
         if platform.system() == 'Windows':
             os.chdir('games/ttcc')
-            subprocess.run(r'./CorporateClash.exe', env=dict(os.environ, TT_GAMESERVER=GAMESERVER, TT_PLAYCOOKIE=COOKIE))
+            print("launcher login is successful")
+            subprocess.Popen(r'./CorporateClash.exe', env=dict(os.environ, TT_GAMESERVER=GAMESERVER, TT_PLAYCOOKIE=COOKIE))
 
 
 
